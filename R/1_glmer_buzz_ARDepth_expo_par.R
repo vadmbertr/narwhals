77f1615ffa89779c8a549ef14146df5840190b38
Vadim <vadim.bertrand@lilo.org>
2022-11-28 14:17:59 +0100

    fix

--- R/1_glmer_buzz_ARDepth_expo_par.R
+++ R/1_glmer_buzz_ARDepth_expo_par.R
@@ -122,12 +122,13 @@ ram.per.job <- 7.5
 n.cores <- detectCores()
 n.cores.alloc <- as.numeric(args[4])
 ram.alloc <- ram.total * n.cores.alloc / n.cores
-n.jobs <- min(n.cores.alloc, floor(ram.alloc / ram.per.job), as.numeric(args[3]) - nrow(expo.coef.all))
+n.jobs <- min(n.cores.alloc,
+              floor(ram.alloc / ram.per.job),
+              as.numeric(args[3]) - n.done(expo.coef.all, n.coefs))
 
 while (as.numeric(args[3]) - n.done(expo.coef.all, n.coefs) > 0) {
   print(n.done(expo.coef.all, n.coefs))
   n.jobs <- min(n.jobs, as.numeric(args[3]) - n.done(expo.coef.all, n.coefs))
-  print(n.jobs)
   expo.coef <- do.call(rbind, mclapply(1:n.jobs, fit.glmer, mc.cores = n.jobs))
   if (is.na(n.coefs)) {
     n.coefs <- as.integer(nrow(expo.coef) / n.jobs)
