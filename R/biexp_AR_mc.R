cdc9d36fc026bd6713159ca48ddc0c9a7692aa42
Vadim <vadim.bertrand@lilo.org>
2022-12-11 20:35:02 +0100

    MC est of biexp

--- R/biexp_AR_mc.R
+++ R/biexp_AR_mc.R
@@ -4,18 +4,15 @@
 
 library(broom)
 library(mvtnorm)
-library(parallel)
-library(RhpcBLASctl)
 source("0_biexp.R")
 
 #---------------------------------------------------------------------------------
 # Read script arguments
 args <- commandArgs(trailingOnly = TRUE) # read args from command line
-if (length(args) != 3) {
-  print("Usage du script : Rscript 1_biexp_AR_mc.R arg1 arg2 arg3")
+if (length(args) != 2) {
+  print("Usage du script : Rscript 1_biexp_AR_mc.R arg1 arg2")
   print("arg1 : le chemin vers le dossier de sauvegarde des objets R")
   print("arg2 : nombre de coefficients estimés souhaités")
-  print("arg3 : le nombre de coeurs alloués") # cannot be retrieve from R
   stop("Des arguments doivent être donnés au script.", call. = FALSE)
 }
 
@@ -29,40 +26,19 @@ glmer.coefs.vcov <- as.matrix(readRDS("../data/glmer_buzz_ARDepth/glmERBuzzARDep
 maxlag.opt <- as.integer(maxlag.bic[which.min(maxlag.bic[, 2]), 1])
 AR.est <- glmer.coefs$estimate[1:(maxlag.opt)+5]
 AR.vcov <- glmer.coefs.vcov[1:(maxlag.opt)+5, 1:(maxlag.opt)+5]
-AR.se <- diag(AR.vcov)
+AR.se <- glmer.coefs$std.error[1:(maxlag.opt)+5]
 
-fit.nls <- function (i) {
+biexp.coef <- replicate(args[2], {
   ARcoefs <- as.numeric(rmvnorm(1, mean = AR.est, sigma = AR.vcov,
                                 checkSymmetry = FALSE))
-  biexp.reg <- nls(ARcoefs ~ SSbiexp(1:maxlag.opt, A1, lrc1, A2, lrc2), weights = 1/AR.se)
-  return(tidy(biexp.reg)[, c("term", "estimate", "std.error")])
-}
+  biexp.reg <- tryCatch({
+    nls(ARcoefs ~ SSbiexp(1:maxlag.opt, A1, lrc1, A2, lrc2),
+                   weights = 1/AR.se)
+  }, error = function (c) return(NULL))
 
-n.done <- function (df, nc) {
-  if (is.na(nc)) return(0)
-  else return(round(nrow(df) / nc))
-}
+  if (is.null(biexp.reg)) return(NULL)
+  else return(as.matrix(tidy(biexp.reg)[, c("term", "estimate", "std.error")]))
+}, simplify = FALSE)
+biexp.coef <- do.call(rbind, biexp.coef)
 
-biexp.coef.path <- paste0(args[2], "/biexp.AR.est.rds")
-if (file.exists(biexp.coef.path)) {
-  biexp.coef.all <- readRDS(biexp.coef.path)
-  n.coefs <- length(unique(biexp.coef.all$term))
-} else {
-  biexp.coef.all <- data.frame()
-  n.coefs <- NA
-}
-
-## Parallelism
-blas_set_num_threads(1) # otherwise some kind of deadlock can occured
-n.jobs <- as.numeric(args[3]) - 1
-
-while (as.numeric(args[2]) - n.done(biexp.coef.all, n.coefs) > 0) {
-  print(n.done(biexp.coef.all, n.coefs))
-  n.jobs <- min(n.jobs, as.numeric(args[2]) - n.done(biexp.coef.all, n.coefs))
-  biexp.coef <- do.call(rbind, mclapply(1:n.jobs, fit.nls, mc.cores = n.jobs))
-  if (is.na(n.coefs)) {
-    n.coefs <- as.integer(nrow(biexp.coef) / n.jobs)
-  }
-  biexp.coef.all <- rbind(expo.coef.all, expo.coef)
-  saveRDS(biexp.coef.all, expo.coef.path)
-}
+saveRDS(biexp.coef, paste0(args[1], "/biexp.AR.est.rds"))
