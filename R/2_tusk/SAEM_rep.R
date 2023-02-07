9812ef7657a2664ad347735ab3b734971aa1bed7
Vadim <vadim.bertrand@lilo.org>
2023-02-07 18:46:15 +0100

    use parallel

--- R/2_tusk/SAEM_rep.R
+++ R/2_tusk/SAEM_rep.R
@@ -2,6 +2,8 @@
 ## Objective : repeat SAEM algo several times and produce error metrics
 #---------------------------------------------------------------------------------
 
+library(parallel)
+library(RhpcBLASctl)
 source("utils/simulation.R")
 source("utils/MCMC.R")
 source("utils/SAEM.R")
@@ -9,32 +11,26 @@ source("utils/SAEM.R")
 #---------------------------------------------------------------------------------
 # Read script arguments
 args <- commandArgs(trailingOnly = TRUE) # read args from command line
-if (length(args) != 2) {
-  print("arg1 : le chemin vers le dossier de sauvegarde des objets R")
-  print("arg2 : le nombre de répétitions de l'expérience")
+if (length(args) != 3) {
+  print("arg1 : le nombre de répétitions de l'expérience")
+  print("arg2 : le nombre de coeurs alloués")
+  print("arg3 : le chemin vers le dossier de sauvegarde des objets R")
   stop("Des arguments doivent être donnés au script.", call. = FALSE)
 }
 
 #---------------------------------------------------------------------------------
 set.seed(15)
 
-# calculation of parameter vectors
-R <- args(args[2])
-omega.vec <- rep(0, R)
-gamma.vec <- rep(0, R)
-psi.vec <- rep(0, R)
-
-for (i in 1:R) {
+run.saem <- function () {
   xi <- rxi(rep(0, n))
   Y <- f(xi) + rnorm(n, 0, omega)
   saem.obj <- saem.alg(Y)
-  omega.vec[[r]] <- saem.obj$omega.c
-  psi.vec[[r]] <- saem.obj$psi.c
-  gamma.vec[[r]] <- saem.obj$gamma.c
+  return(data.frame(omega = saem.obj$omega.c, psi = saem.obj$psi.c, gamma = saem.obj$gamma.c,
+                    A = saem.obj$A.c, B = saem.obj$B.c, a = saem.obj$a.c, b = saem.obj$b.c))
 }
 
-#---------------------------------------------------------------------------------
-# Save R objects
-saveRDS(omega.vec, paste0(args[1], "/omega.vec.rds"))
-saveRDS(psi.vec, paste0(args[1], "/psi.Vec.rds"))
-saveRDS(gamma.vec, paste0(args[1], "/gamma.Vec.rds"))
+n.rep <- args[[1]]
+blas_set_num_threads(1)
+n.jobs <- args[[2]]
+parameters.est <- do.call(rbind(mclapply(1:n.rep, run.saem, mc.cores = n.jobs)))
+saveRDS(parameters.est, paste0(args[[3]], "/parameters.est.rds"))
